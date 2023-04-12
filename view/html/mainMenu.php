9d00bb8bf18c8624426af6291f72d77920991728
Cuerbu <lai0nn@outlook.com>
2023-04-12 09:25:04 -0600

    cambios en menu

--- view/html/mainMenu.php
+++ view/html/mainMenu.php
@@ -1,4 +1,4 @@
-<div class="br-logo"><a href=""><span>[</span>CERTIUM<span>]</span></a></div>
+<div class="br-logo"><a href="../UsuarioInicio/"><span>[</span>CERTIUM<span>]</span></a></div>
     <div class="br-sideleft overflow-y-auto">
       <label class="sidebar-label pd-x-15 mg-t-20">Menu</label>
       <div class="br-sideleft-menu">
@@ -18,6 +18,14 @@
           <div class="br-menu-item">
             <i class="menu-item-icon icon ion-ios-gear-outline tx-20"></i>
             <span class="menu-item-label">Perfil</span>
+          </div><!-- menu-item -->
+        </a><!-- br-menu-link -->
+
+
+        <a href="../UsuarioPerfil" class="br-menu-link">
+          <div class="br-menu-item">
+            <i class="menu-item-icon icon ion-power tx-20"></i>
+            <span class="menu-item-label">Cerrar sesión</span>
           </div><!-- menu-item -->
         </a><!-- br-menu-link -->
        
