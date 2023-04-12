c9cddbe6271d8c970ed5a20656ce2679c9bd74ab
Cuerbu <lai0nn@outlook.com>
2023-04-12 09:19:02 -0600

    compactando blank

--- view/UsuarioCurso/index.php
+++ view/UsuarioCurso/index.php
@@ -1,1 +1,58 @@
-<h1>usuario curso</h1>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <?php
+    require_once("../html/mainHead.php");
+  ?>
+    <title>Inicio</title>
+</head>
+
+  <body>
+
+    <!-- ########## START: LEFT PANEL ########## -->
+   <?php
+  require_once("../html/mainMenu.php");
+  ?>
+
+      <br>
+    </div><!-- br-sideleft -->
+    <!-- ########## END: LEFT PANEL ########## -->
+
+    <!-- ########## START: HEAD PANEL ########## -->
+<?php
+    require_once("../../view/html/mainHeader.php")
+
+?>
+    <!-- ########## END: HEAD PANEL ########## -->
+
+    <!-- ########## START: RIGHT PANEL ########## -->
+    
+    <!-- ########## END: RIGHT PANEL ########## --->
+
+    <!-- ########## START: MAIN PANEL ########## -->
+    <div class="br-mainpanel">
+      <div class="br-pageheader pd-y-15 pd-l-20">
+        <nav class="breadcrumb pd-0 mg-0 tx-12">
+          <a class="breadcrumb-item" href="#">Curso</a>
+        </nav>
+      </div><!-- br-pageheader -->
+      <div class="pd-x-20 pd-sm-x-30 pd-t-20 pd-sm-t-30">
+        <h4 class="tx-gray-800 mg-b-5">Curso</h4>
+        <p class="mg-b-0">Pantalla Curso.</p>
+      </div>
+
+      <div class="br-pagebody">
+
+        <!-- start you own content here -->
+
+      </div><!-- br-pagebody -->
+
+    </div><!-- br-mainpanel -->
+    <!-- ########## END: MAIN PANEL ########## -->
+
+   <?php
+   require_once ("../../view/html/mainJs.php");
+   ?>
+   
+  </body>
+</html>
--- view/html/mainHeader.php
+++ view/html/mainHeader.php
@@ -1,25 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-  <?php
-    require_once("../html/mainHead.php");
-  ?>
-    <title>Inicio</title>
-</head>
-
-  <body>
-
-    <!-- ########## START: LEFT PANEL ########## -->
-   <?php
-  require_once("../html/mainMenu.php");
-  ?>
-
-      <br>
-    </div><!-- br-sideleft -->
-    <!-- ########## END: LEFT PANEL ########## -->
-
-    <!-- ########## START: HEAD PANEL ########## -->
-    <div class="br-header">
+<div class="br-header">
       <div class="br-header-left">
         <div class="navicon-left hidden-md-down"><a id="btnLeftMenu" href=""><i class="icon ion-navicon-round"></i></a></div>
         <div class="navicon-left hidden-lg-up"><a id="btnLeftMenuMobile" href=""><i class="icon ion-navicon-round"></i></a></div>
@@ -54,39 +33,3 @@
        
       </div><!-- br-header-right -->
     </div><!-- br-header -->
-    <!-- ########## END: HEAD PANEL ########## -->
-    <?php
-    require_once("../../view/html/mainHeader.php")
-
-?>
-    <!-- ########## START: RIGHT PANEL ########## -->
-    
-    <!-- ########## END: RIGHT PANEL ########## --->
-
-    <!-- ########## START: MAIN PANEL ########## -->
-    <div class="br-mainpanel">
-      <div class="br-pageheader pd-y-15 pd-l-20">
-        <nav class="breadcrumb pd-0 mg-0 tx-12">
-          <a class="breadcrumb-item" href="#">Inicio</a>
-        </nav>
-      </div><!-- br-pageheader -->
-      <div class="pd-x-20 pd-sm-x-30 pd-t-20 pd-sm-t-30">
-        <h4 class="tx-gray-800 mg-b-5">Inicio</h4>
-        <p class="mg-b-0">Pantalla Inicio.</p>
-      </div>
-
-      <div class="br-pagebody">
-
-        <!-- start you own content here -->
-
-      </div><!-- br-pagebody -->
-
-    </div><!-- br-mainpanel -->
-    <!-- ########## END: MAIN PANEL ########## -->
-
-   <?php
-   require_once ("../../view/html/mainJs.php");
-   ?>
-   
-  </body>
-</html>
